30e9f0745247c276cf3ad6107461ea5f357c9d60
AlinaMoskieva <alina.moskieva@flatstack.com>
2019-12-09 19:52:13 +0100

    Refactor feedbacks crawler

--- tripadvisor_crawler/spiders/feedbacks_spider.py
+++ tripadvisor_crawler/spiders/feedbacks_spider.py
@@ -4,7 +4,6 @@ import re
 
 from bs4 import BeautifulSoup
 
-
 class FeedbacksSpider(scrapy.Spider):
     name = "feedbacks"
 
@@ -14,9 +13,8 @@ class FeedbacksSpider(scrapy.Spider):
     def start_requests(self):
         with open(self.restaurants_filename, "r") as restaurants_file:
             datastore = json.load(restaurants_file)
-            # print("ok")
+
             for restaurant in datastore:
-                # print(restaurant["url"])
                 yield scrapy.Request(url=restaurant["url"], callback=self.parse)
 
     def parse(self, response):
@@ -39,19 +37,16 @@ class FeedbacksSpider(scrapy.Spider):
         reviews_data = {}
 
         for review in reviews:
-            reviews_data["username"] = ""
-            reviews_data["review_likes_amount"] = ""
-            reviews_data["date"] = ""
-            reviews_data["review_title"] = ""
-            reviews_data["review_body"] = ""
-            reviews_data["date_of_visit"] = ""
-            user_info = review.find("div", class_="pointer_cursor")
-            # reviews_data["user_location"] = user_info.find("div", class_="userLoc").find("strong").get_text()
-            reviews_data[
-                "username"
-            ] = user_info.get_text()  # .replace(reviews_data["user_location"], "")
+            user_info = review.find("div", class_="pointer_cursor") or ""
+            user_location_info =  user_info.find("div", class_="userLoc") or ""
+
+            reviews_data["user_location"] = user_location_info.find("strong").get_text() if user_location_info else ""
+            reviews_data["username"] = user_info.get_text().replace(reviews_data["user_location"], "")
+            reviews_data["date"] = review.find("span", class_="ratingDate").get_text() or ""
+
             user_badging_block = review.find("div", class_="memberBadgingNoText")
             badgings = user_badging_block.findAll("span", class_="badgetext")
+            reviews_data["review_likes_amount"] = ""
 
             if user_badging_block.find("span", class_="pencil-paper"):
                 reviews_data["reviews_by_user_amount"] = badgings[0].get_text()
@@ -62,26 +57,21 @@ class FeedbacksSpider(scrapy.Spider):
             elif user_badging_block.find("span", class_="thumbs-up-fill"):
                 reviews_data["review_likes_amount"] = badgings[0].get_text()
 
-            reviews_data["date"] = review.find("span", class_="ratingDate").get_text()
-            reviews_data["review_title"] = review.find(
-                "span", class_="noQuotes"
-            ).get_text()
-            reviews_data["review_body"] = review.find(
-                "p", class_="partial_entry"
-            ).get_text()
-            reviews_data["date_of_visit"] = review.find(
-                "div", class_="prw_reviews_stay_date_hsx"
-            ).get_text()
+            reviews_data["review_title"] = review.find("span", class_="noQuotes").get_text()
+            reviews_data["review_body"] = review.find("p", class_="partial_entry").get_text()
+            reviews_data["date_of_visit"] = review.find("div", class_="prw_reviews_stay_date_hsx").get_text()
+
             yield {
                 "username": reviews_data["username"],
+                "location": reviews_data["user_location"],
                 "review_likes_amount": reviews_data["review_likes_amount"],
                 "date": reviews_data["date"],
                 "review_title": reviews_data["review_title"],
                 "review_body": reviews_data["review_body"],
-                "date_of_visit": reviews_data["date_of_visit"]
-                # "review_count": soup.find("span", class_="reviews_header_count").get_text(),
-                # "assessments_info": assessments_data,
-                # "reviews": reviews_data
+                "date_of_visit": reviews_data["date_of_visit"],
+                "review_count": soup.find("span", class_="reviews_header_count").get_text(),
+                "assessments_info": assessments_data,
+                "reviews": reviews_data
             }
 
             next_page = home_url + response.css('.nav.next ::attr("href")').get()
